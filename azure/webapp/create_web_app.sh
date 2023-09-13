9718aa40a605a4511d5adbdb1aab0e30cb3a20a4
SzymonJakubiak <szymonjakubiak@gmail.com>
2023-09-13 06:20:01 +0000

    Move webapp scripts to a separate folder

--- azure/webapp/create_web_app.sh
+++ azure/webapp/create_web_app.sh
@@ -6,7 +6,7 @@ RG_NAME="rg_fastapi_webapp"
 APP_PLAN_NAME="fastapi-webapp-plan"
 APP_NAME="fastapi-webapp-example-app"
 RUNTIME="PYTHON:3.9"
-STARTUP_FILE="azure/startup_file.sh"
+STARTUP_FILE="azure/webapp/startup_file.sh"
 
 az group create --location $LOCATION --name $RG_NAME \
 && az appservice plan create --name $APP_PLAN_NAME --resource-group $RG_NAME --is-linux --sku "F1" \
